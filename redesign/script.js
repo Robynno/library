0faa13ff0bdc60f9f5b6983c2e1d509e0051f6b1
Robynno <jecroisrobyn@gmail.com>
2025-03-20 23:30:02 -0400

    damn

--- redesign/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-  var dropdown = document.getElementById("library-hours-dropdown");
-  if (dropdown.style.display === "none" || dropdown.style.display === "") {
-    dropdown.style.display = "block";
-  } else {
-    dropdown.style.display = "none";
-  }
-}
